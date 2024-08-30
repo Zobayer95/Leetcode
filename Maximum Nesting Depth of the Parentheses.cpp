12ee0a46c37d928622e45f260d929cd2823df5d9
Zobayer Abdullah <zobayer.abdullah9999@gmail.com>
2024-08-30 22:46:35 +0600

    Add files via upload

---
+++ Maximum Nesting Depth of the Parentheses.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+  int maxDepth(string s) {
+    int ans = 0;
+    int d = 0;
+    for (char c : s) {
+      if (c == '(') ans = max(ans, ++d);
+      else if (c == ')') --d;
+    }
+    return ans;
+  }
+};
